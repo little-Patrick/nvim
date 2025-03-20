6054ca8f1ef73b6912111e51bef82667542c1df0
little-Patrick <Little.patrick@outlook.com>
2025-03-20 08:29:15 -0600

    commitment issues

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,7 +1,18 @@
 
 vim.cmd("set tabstop=4")
 
-vim.keymap.set("n", "<leader>r", "<cmd>!go run %<CR>", { silent = true })  -- Run the entire Go file
-vim.keymap.set("n", "<leader>R", ":.w !go test -run .%<CR>", { silent = true })  -- Run tests in the current file
-vim.keymap.set("v", "<leader>R", ":'<,'>w !go run<CR>", { silent = true })  -- Run selected lines in visual mode
+-- vim.keymap.set("n", "<leader>r", "<cmd>!go run %<CR>", { silent = true })  -- Run the entire Go file
+-- vim.keymap.set("n", "<leader>R", ":.w !go test -run .%<CR>", { silent = true })  -- Run tests in the current file
+-- vim.keymap.set("v", "<leader>R", ":'<,'>w !go run<CR>", { silent = true })  -- Run selected lines in visual mode
+-- vim.keymap.set("n", "<leader>c", 'ofmt.Println("")<Esc>hi', { noremap = true, silent = true })
 
+-- Run entire Go file
+vim.keymap.set("n", "<leader>r", "<cmd>!go run %<CR>", { silent = true })
+-- Run current line with Yaegi
+vim.keymap.set("n", "<leader>R", '<cmd>!dlv debug', { silent = true })
+-- Run selected lines in visual mode with Yaegi
+vim.keymap.set("v", "<leader>R", ":'<,'>w !yaegi<CR>", { silent = true })
+-- Insert a quick fmt.Println() debugging statement
+vim.keymap.set("n", "<leader>c", 'ofmt.Println("")<Esc>hi', { noremap = true, silent = true })
+-- Insert a runtime.Breakpoint() call for debugging
+vim.keymap.set("n", "<leader>C", 'oimport "runtime"; runtime.Breakpoint()<Esc>', { noremap = true, silent = true })
